d0225686387b91c88a052736949619e642944874
wee kiat <getkiat@gmail.com>
2022-10-22 23:40:09 +0800

    Project outline

---
+++ Classes/Windlog.hpp
@@ -0,0 +1,19 @@
+#ifndef WINDLOG_HPP
+#define WINDLOG_HPP
+
+#include "Date.hpp"
+#include "Time.hpp"
+
+// Abstracts a line of windlog data
+class Windlog
+{
+public:
+    //.....
+private:
+    //.....
+    Date date;
+    Time time;
+    float speed;
+};
+
+#endif
